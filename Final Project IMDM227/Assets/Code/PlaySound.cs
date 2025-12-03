70b15bed30c0e9a46d17d817f918b38156d66e88
MirWasHere <mirwashere@users.noreply.github.com>
2025-12-03 18:32:54 -0500

    Added sound affect to collectible

--- Final Project IMDM227/Assets/Code/PlaySound.cs
+++ Final Project IMDM227/Assets/Code/PlaySound.cs
@@ -4,6 +4,8 @@ public class PlaySound : MonoBehaviour
 {
     AudioSource audSource;
     AudioClip clip;
+
+    public GameObject gameObj;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +22,8 @@ public class PlaySound : MonoBehaviour
     // When the object is collided with
     private void OnTriggerEnter(Collider other)
     {
-        // SetActive(false);
+        audSource.Play(0);
+        Debug.Log("Playing note...");
+        gameObj.SetActive(false);
     }
 }
