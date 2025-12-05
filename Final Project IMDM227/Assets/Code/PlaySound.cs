f4a15f1e889dae8e9e55e0ad471b0b2e2007f44a
MirWasHere <mirwashere@users.noreply.github.com>
2025-12-05 09:25:22 -0500

    Creating array of notes
    The array will be attached to the player, and play the missing notes the player collects

--- Final Project IMDM227/Assets/Code/PlaySound.cs
+++ Final Project IMDM227/Assets/Code/PlaySound.cs
@@ -1,28 +1,55 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlaySound : MonoBehaviour
 {
     AudioSource audSource;
-    AudioClip clip;
+
+    private IEnumerator coroutine;
 
     public GameObject gameObj;
+    PlayerMovement playMove;
+
+    // Tracks index, will be the same as collectible count (minus 1)
+    int index = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Sound!");
+        // Reference to player object
+        gameObj = GameObject.FindWithTag("Player");
+        // Getting a reference to player's playerMovement to get clips array
+        playMove = GetComponent<PlayerMovement>();
+        // Getting audio source
         audSource = GetComponent<AudioSource>();
+        // intializing coroutine
+        coroutine = WaitForNote(1.6f);
     }
 
     // When the object is collided with
     private void OnTriggerEnter(Collider other)
     {
+        // // Getting next note from array
+        // audSource.clip = playMove.clips[index];
+        Debug.Log("note played: " + audSource.clip);
         // Play note...
         audSource.Play();
         Debug.Log("Playing note...");
 
         // Waiting for sound to finish before playing sound
-        /**/
+        StartCoroutine(coroutine);
 
+        // Increment index
+        // index ++;
+    }
+
+
+    // Creating a coroutine, a way to pause code
+    IEnumerator WaitForNote(float waitTime)
+    {
+        // Pausing for input seconds
+        yield return new WaitForSeconds(waitTime);
+
+        Debug.Log("Finished waiting, remove collectible.");
         // Setting object inactive
         gameObj.SetActive(false);
     }
--- Final Project IMDM227/Assets/Code/PlayerMovement.cs
+++ Final Project IMDM227/Assets/Code/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    // Array of notes; will be shared with play sound
+    public AudioClip[] clips;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
