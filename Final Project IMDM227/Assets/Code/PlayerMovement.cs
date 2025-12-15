5d792dd0e8e111185faeaac2f60a0ea7bab5ecf1
MirWasHere <mirwashere@users.noreply.github.com>
2025-12-15 08:10:44 -0500

    fixed colliders, going to add some to stairs?

--- Final Project IMDM227/Assets/Code/PlayerMovement.cs
+++ Final Project IMDM227/Assets/Code/PlayerMovement.cs
@@ -28,26 +28,38 @@ public class PlayerMovement : MonoBehaviour
         {
             rate = -60;
             transform.Rotate(0, rate * Time.deltaTime, 0);
+            Debug.Log("Left!");
         }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
             rate = 60;
             transform.Rotate(0, rate * Time.deltaTime, 0);
+            Debug.Log("Right!");
         }
 
         if (Keyboard.current.upArrowKey.isPressed)
         {
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
             speed = 5;
             transform.Translate(Time.deltaTime * speed * Vector3.forward);
+            Debug.Log("Forward!");
         }
         else if (Keyboard.current.downArrowKey.isPressed)
         {
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
             speed = -5;
             transform.Translate(Time.deltaTime * speed * Vector3.forward);
+            Debug.Log("Back!");
+        }
+        else
+        {
+            // Freeze rotation if not in use
+            rb.constraints |= RigidbodyConstraints.FreezeRotationY;
         }
 
         if(Keyboard.current.spaceKey.isPressed){
             transform.Translate(Vector3.up * Time.deltaTime * power);
+            Debug.Log("Jump!");
         }
     }
 }
